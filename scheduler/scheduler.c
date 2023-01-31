9502d55bfdc531595c79172fa48f3d1248f2afaa
JohnnyKyr <kuriako1234@gmail.com>
2023-01-31 23:02:26 +0200

    Update scheduler.c

--- scheduler/scheduler.c
+++ scheduler/scheduler.c
@@ -13,19 +13,12 @@
 /* header files */
 
 /* global definitions */
-//add shared memory for time
 static volatile int finishedProcFlag = 0;
 
 /* definition and implementation of process descriptor and queue(s) */
 
-
-struct msg_buffer{
-	long mesg_type;
-	struct queue *q;
-};
-
-typedef struct msg_buffer message;
 typedef struct MinHeap MinHeap;
+
 struct MinHeap {
     struct process *p;
     int size;
@@ -52,23 +45,73 @@ struct time {
 	double _2;
 };
 
-
-void push(struct queue **head,struct process *PROCC){
-	
-	struct queue* Q = (struct queue*)malloc(sizeof(struct queue));
-	//head->1->2->end 
-	Q->p = PROCC;
-	//printf("%s",Q->p->name);
-	Q->next = (*head);
-	Q->prev = NULL;
-
-
-	if ((*head) != NULL)
-		(*head)->prev = Q;
-	
-	(*head) = Q;
+double get_wtime(void)
+{
+  struct timeval t;
+  gettimeofday(&t, NULL);
+  return (double)t.tv_sec + (double)t.tv_usec*1.0e-6;
 }
 
+
+int numOfProcess(FILE *fp){
+	
+	int count =0;
+	char c;
+	
+	if (fp==NULL) fprintf(stderr,"ERROR1");
+	fseek(fp,0,SEEK_SET);
+    c = getc(fp);
+	while(c!=EOF){
+		if (c == '\n') count++;
+	
+		c = getc(fp);
+	}
+	return count;
+}
+
+void getProcess(FILE *fp,int n,struct process *PROCCS){
+   if(fp==NULL) fprintf(stderr,"ERROR2");
+    int k=0;
+    int *numOfChars;
+   
+    int n1=0;
+
+    char c;
+   
+    char *buf;
+    
+    fseek(fp,0,SEEK_SET);
+
+
+    numOfChars=(int*)malloc(n*sizeof(int));// A dynamic array that counts the chars before '}'
+    numOfChars[k]=0;
+
+    c = getc(fp);
+    
+    while(c!=EOF){
+
+        if(c != '\n')  numOfChars[k]++; // It counts the chars before '}'/
+            else if(c=='\n') k++;   //The "i" takes numOfChars in the next element of the array
+
+        c = getc(fp);//Restarts the pointer fp1
+    }
+    
+    int i=0;
+  
+    fseek(fp,0,SEEK_SET);
+   
+
+    while(i<n){
+		
+        buf=malloc(numOfChars[i]*sizeof(char)+2);
+        fscanf(fp,"%s %d",buf,&n1);//Read the file until \n    
+		
+		PROCCS[i].data = n1;
+		PROCCS[i].name = buf;
+		PROCCS[i].pid = 0;
+		i++;
+	}
+}
 
 int parent(int i) {return (i - 1) / 2;}
 int left_child(int i){return (2*i + 1);}
@@ -144,6 +187,7 @@ MinHeap* heapify(MinHeap* heap, int index) {
 
     return heap;
 }
+
 void print_heap(MinHeap* heap) {
     
     printf("Min Heap:\n");
@@ -151,6 +195,22 @@ void print_heap(MinHeap* heap) {
         printf("%d -> ", heap->p[i].data);
     }
 	printf("\n");
+}
+
+void push(struct queue **head,struct process *PROCC){
+	
+	struct queue* Q = (struct queue*)malloc(sizeof(struct queue));
+	//head->1->2->end 
+	Q->p = PROCC;
+	//printf("%s",Q->p->name);
+	Q->next = (*head);
+	Q->prev = NULL;
+
+
+	if ((*head) != NULL)
+		(*head)->prev = Q;
+	
+	(*head) = Q;
 }
 
 void pop(struct queue **head)
@@ -178,70 +238,6 @@ void printList(struct queue* node){
 		
 		node = node->prev;
 	}
-	
-	
-}
-
-int numOfProcess(FILE *fp){
-	
-	int count =0;
-	char c;
-	
-	if (fp==NULL) fprintf(stderr,"ERROR1");
-	fseek(fp,0,SEEK_SET);
-    c = getc(fp);
-	while(c!=EOF){
-		if (c == '\n') count++;
-	
-		c = getc(fp);
-	}
-	return count;
-}
-
-void getProcess(FILE *fp,int n,struct process *PROCCS){
-   if(fp==NULL) fprintf(stderr,"ERROR2");
-    int k=0;
-    int *numOfChars;
-   
-    int n1=0;
-
-    char c;
-   
-    char *buf;
-    
-    fseek(fp,0,SEEK_SET);
-
-
-    numOfChars=(int*)malloc(n*sizeof(int));// A dynamic array that counts the chars before '}'
-    numOfChars[k]=0;
-
-    c = getc(fp);
-    
-    while(c!=EOF){
-
-        if(c != '\n')  numOfChars[k]++; // It counts the chars before '}'/
-            else if(c=='\n') k++;   //The "i" takes numOfChars in the next element of the array
-
-        c = getc(fp);//Restarts the pointer fp1
-    }
-    
-    int i=0;
-  
-    fseek(fp,0,SEEK_SET);
-   
-
-    while(i<n){
-		
-        buf=malloc(numOfChars[i]*sizeof(char)+2);
-        fscanf(fp,"%s %d",buf,&n1);//Read the file until \n    
-		
-		PROCCS[i].data = n1;
-		PROCCS[i].name = buf;
-		PROCCS[i].pid = 0;
-		i++;
-	}
-
- 
 }
 
 struct queue * gotoHead(struct queue* q,int n ){
@@ -258,35 +254,33 @@ struct queue * gotoTail(struct queue* q, int n ){
 	}
 	return q;
 }
+
+struct queue * pushHeadtoTail(struct queue *requestQ,int n){
+	
+		if (n<=1) return requestQ;
+		struct process *procc = requestQ->p;
+		pop(&requestQ);
+		
+		requestQ = gotoTail(requestQ,n-1);
+		push(&requestQ,procc);
+		requestQ=gotoHead(requestQ,n);
+		return requestQ;
+}
+
 void sigchld_handler(int signo){
 	int pid, status;
 	
-	
-
 	while ((pid = waitpid(-1, &status, WNOHANG)) > 0){
 		finishedProcFlag=1;
 	}
-	
-
-	
 }
-
-double get_wtime(void)
-{
-  struct timeval t;
-  gettimeofday(&t, NULL);
-  return (double)t.tv_sec + (double)t.tv_usec*1.0e-6;
-}
-
-
 
 void _static(struct queue *q,int flag){
 	int pid;
-	
-	key_t key;
-	
+	key_t key;	
 	struct time *time;
 	struct sigaction sact;
+
 	key = ftok("../scheduler", 65);
 	sact.sa_handler = sigchld_handler;
 	sigemptyset(&sact.sa_mask);
@@ -319,21 +313,16 @@ void _static(struct queue *q,int flag){
 	if (flag==1)
 	printf("WORKLOAD TIME: %f seconds\n",time->_2-time1);
 }
-struct queue * pushHeadtoTail(struct queue *requestQ,int n){
-	
-		if (n<=1) return requestQ;
-		struct process *procc = requestQ->p;
-		pop(&requestQ);
-		
-		requestQ = gotoTail(requestQ,n-1);
-		push(&requestQ,procc);
-		requestQ=gotoHead(requestQ,n);
-		return requestQ;
-}
+
 
 void rr(struct queue *q,float quantum,int size,int flag){
 	struct time *time;
     struct sigaction sa;
+	struct queue *requestQ=NULL;
+	int n =0;
+	pid_t pid;
+	int fd = shm_open("/my_shared_struct", O_CREAT | O_RDWR, 0666);
+	//struct timespec rqtp,rmtp = {0,quantum};
     sa.sa_handler = sigchld_handler;
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = 0;
@@ -342,104 +331,88 @@ void rr(struct queue *q,float quantum,int size,int flag){
     sigemptyset(&mask);
     sigaddset(&mask, SIGCHLD);
     sigprocmask(SIG_BLOCK, &mask, NULL);
+	float t_quantum = quantum/1000;
 	
-	struct queue *requestQ=NULL;
-	int n =0;
-	message message;
-	int fd = shm_open("/my_shared_struct", O_CREAT | O_RDWR, 0666);
-
+	//Shared memory is used only for process to have access to the time and pid values
     // Set the size of the shared memory region
     ftruncate(fd, sizeof(struct queue*));
-
     // Map the shared memory region to a pointer
-    requestQ = mmap(NULL, sizeof(struct queue*), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-
-	int pid; // process id
-	
-	
+    requestQ = mmap(NULL, sizeof(struct queue*), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);		
 	// Set the size of the shared memory region
     ftruncate(fd, sizeof(struct time*));
-
     // Map the shared memory region to a pointer
     time = mmap(NULL, sizeof(struct time*), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-
-	float t_quantum = quantum/1000;
-	struct timespec rqtp,rmtp = {0,quantum};
-	
 	time->_1=get_wtime();
 	while(1){
-		
-			
+
 			while( q!=NULL  ) {
-				
+				//add process to requestQ
 				requestQ= gotoTail(requestQ,n);
 				push(&requestQ,q->p);
 				n++;
 				requestQ= gotoHead(requestQ,n);
 				pop(&q);
 				}
-				
 				if(requestQ->p->pid==0){ 
-					
+					//if command has no pid
 					pid =fork();
-					
-					
 					if(pid>0){
-						 
+ 
 						requestQ->p->pid = pid;
 						requestQ->p->time2 =0;
 						requestQ->p->time1 = get_wtime();
 						requestQ->p->time2 =get_wtime();
 						sleep(1);
 						kill(requestQ->p->pid,SIGSTOP);
-						
 						requestQ = pushHeadtoTail(requestQ,n);
-						
 					}
 					if(pid==0){
-						
 						execl(requestQ->p->name,NULL);
 						exit(1);
 					}
-					
 				}		
 				else if(requestQ!=NULL && requestQ->p->pid!=0){
 					double time1,time2;
+					//block SIGCHLD
 					sigprocmask(SIG_BLOCK, &mask, NULL);
 
 					time1 =get_wtime();
+					//Continue the process 
 					kill(requestQ->p->pid,SIGCONT);
 					sleep(1);
+					//Stop the process
 					kill(requestQ->p->pid,SIGSTOP);
+					//Unblock the process inorder to receive SIGCHLD signals
 					sigprocmask(SIG_UNBLOCK, &mask, NULL);
-					
 					time2 = get_wtime();
-					
+
 					double ntime = time2-time1;
 					requestQ->p->time2 += ntime;
+
 					if(finishedProcFlag==1&&requestQ!=NULL){
+						//if finishedProcFlag ==1 then a process has finished
 						printf("PID %d - CMD: %s\n",requestQ->p->pid,requestQ->p->name);
 						printf("\t\t\t\t Elapsed time = %f\n",requestQ->p->time2-requestQ->p->time1);
 						time->_2 = get_wtime() ;
+
+						//flag is only for output purposes
 						if(flag==1)
 						printf("\t\t\t\t Workload time = %f\n",  time->_2-time->_1);
 						pop(&requestQ);
 						n--;
 						finishedProcFlag=0;
 				}else{
-						
+					//After the the execution of a process add it to the tail
 					requestQ =pushHeadtoTail(requestQ,n);
 					}
-					}
-					
-			if (n==0)break;	
-				
-	
+				}
+			if (n==0)break;	// Escape if no process in requestQ
 	}
-	
+	//clear the shared memory
 	munmap(requestQ, sizeof(struct queue *));
+
+	//flag is only for output purposes
 	if (flag==1)
 	printf("WORKLOAD TIME: %f seconds\n",time->_2-time->_1);
 
@@ -453,7 +426,7 @@ void prio(struct queue *q,int quantum,int size){
 	while(1){
 		struct queue *priorityqueue=NULL;
 		while( q!=NULL && q->p->data<=priority ) {
-				
+				//add the elements to the queue with the specified number
 				priorityqueue= gotoTail(priorityqueue,n);
 				push(&priorityqueue,q->p);
 				n++;
@@ -462,14 +435,18 @@ void prio(struct queue *q,int quantum,int size){
 				}
 				
 		if(n==1){		
+			//if there is only one cmds in priority queue, 
+			//run it with static scheduling algorithm
 			_static(priorityqueue,0);
 			n=0;
 
 		}
 		else if (n>1){
+			//if more than one cmds, run RR
 			rr(priorityqueue,quantum,n,0);
 			n=0;
 		}
+
 		priority++;
 		if(q==NULL&&priorityqueue==NULL) break;
 	}
@@ -479,20 +456,15 @@ void prio(struct queue *q,int quantum,int size){
 }
 int main(int argc,char **argv)
 {
-	double time_1, time_2;
+	
 	struct process* PROCCS =NULL;
 	struct queue *Q =NULL;
-	
 	FILE *fp;
-	
 	char * FILENAME;
 	int n;
 	float quantum;
 	sscanf(argv[2], "%f", &quantum);
-		
-	
 
-	
 	FILENAME = argv[argc-1];
 	fp = fopen(FILENAME,"r");
 	
@@ -509,9 +481,9 @@ int main(int argc,char **argv)
 		for(int i=0;i<n;i++) push(&Q,&PROCCS[i]);
 		
 		Q = gotoHead(Q,n);
-		time_1 = get_wtime();
+		
 		_static(Q,1);
-		time_2 = get_wtime();
+		
  		
 	}
 	else if(strcmp(argv[1],"SJF")==0){
@@ -527,18 +499,18 @@ int main(int argc,char **argv)
 	}
 		
 		Q = gotoHead(Q,n);
-		time_1 = get_wtime();
+		
 		_static(Q,1);
-		time_2 = get_wtime();
+		
  
 	}else if(strcmp(argv[1],"RR")==0){
 		
 		for(int i=0;i<n;i++) push(&Q,&PROCCS[i]);
 		
 		Q = gotoHead(Q,n);
-		time_1 = get_wtime();
+	
 		rr(Q,quantum,n,1);
-		time_2 = get_wtime();
+	
  		
 
 	}else if(strcmp(argv[1],"PRIO")==0){
@@ -550,19 +522,9 @@ int main(int argc,char **argv)
 		(*temp) = heap->p[0];
 		push(&Q,temp);
 		heap = delete_minimum(heap);
-		
 		}
-		
 		Q = gotoHead(Q,n);
-		
-		time_1 = get_wtime();
 		prio(Q,quantum,n);
-		time_2 = get_wtime();
- 		
 
 	}
-
-
-	
 }
-
